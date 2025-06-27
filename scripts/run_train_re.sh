c13631bf14b14a51ab73d763cca65323e333a128
sirapop3 <112592249+sirapop3@users.noreply.github.com>
2025-06-27 18:44:19 -0500

    Update run_train_re.sh to use local model and take new data

--- scripts/run_train_re.sh
+++ scripts/run_train_re.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-source /jet/home/ghong1/miniconda3/bin/activate PL-Marker
-echo "Activated PL-Marker"
+#source /jet/home/ghong1/miniconda3/bin/activate PL-Marker
+#echo "Activated PL-Marker"
 
-# --use_ner_results: use the original entity type predicted by NER models
-# DiMB-RE
-# mkdir dimb-re_models
-
-# split=dev
+--use_ner_results: use the original entity type predicted by NER models
+DiMB-RE
+mkdir dimb-re_models
+split=dev
 split=test
 
-### SET YOUR OWN OUTPUT DIR ###
-# output_dir=/jet/home/ghong1/ocean_cis230030p/ghong1/PL-Marker/no-result
+SET YOUR OWN OUTPUT DIR
+output_dir=/jet/home/ghong1/ocean_cis230030p/ghong1/PL-Marker/no-result
 output_dir=./output
 
 for seed in 0 1 2 3 4; do 
     python3 run_re_trg_inserted.py \
     --model_type bertsub \
-    --model_name_or_path /jet/home/ghong1/ocean_cis230030p/ghong1/BiomedBERT-fulltext \
+    --model_name_or_path /home/user/combiniiii/COMBINI_PL-Marker-main/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext \
     --do_lower_case \
-    --data_dir ../data/DiMB-RE/ner_reduced_v6.1_trg_abs_result \
+    --data_dir ./data \
     --learning_rate 2e-5 --num_train_epochs 9 \
     --per_gpu_train_batch_size 8 \
     --per_gpu_eval_batch_size 16 \
@@ -29,17 +28,15 @@ for seed in 0 1 2 3 4; do
     --save_steps 607 \
     --eval_logsoftmax \
     --seed $seed \
-    --output_dir $output_dir/RE/dimb-re_models_biomedbert_trg_inserted_${split}-${seed} \
-    --test_file $output_dir/NER/dimb-re_models_biomedbert_trg_${split}-${seed}/ent_pred_${split}.json \
+    --output_dir $output_dir/RE/dimb-re_models_biomedbert_trg_test-${seed} \
     --overwrite_output_dir \
-    --use_ner_results \
     --use_trigger \
     --use_typemarker \
     --lminit \
     --eval_all_checkpoints \
     --binary_cls \
-    --no_sym
-    # --do_train
+    --no_sym \
+    --do_train
     # --do_eval \
     # --evaluate_during_training \
     # --no_test
